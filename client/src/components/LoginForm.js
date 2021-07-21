715f188b64219abc4b286f40b76e70651a17f1ce
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-21 19:52:32 +0400

    login and signup mutation functionality added

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import {useMutation} from '@apollo/client';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
@@ -9,6 +9,9 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [loginUser, { error } ] = useMutation(LOGIN_USER)
+
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,15 +29,8 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await loginUser({variables: { ...userFormData }});
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -49,6 +45,7 @@ const LoginForm = () => {
 
   return (
     <>
+      {error && <div>Something went wrong...</div>}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your login credentials!
